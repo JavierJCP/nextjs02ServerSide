729b2c8597418328cef15f108ccbe6e206dd2773
Javier Casafranca <@JavierJCP>
2026-01-17 19:33:07 -0500

    server side renderin

---
+++ app/components/SidebarMenuItem.tsx
@@ -0,0 +1,30 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { JSX } from "react";
+import style from "./SidevarMenuItem.module.css";
+
+interface Props {
+  path: string;
+  icon: JSX.Element;
+  title: string;
+  subTitle: string;
+}
+
+export function SidebarMenuItems({ path, icon, title, subTitle }: Props) {
+  const pathName = usePathname();
+  return (
+    <Link
+      href={path}
+      className={`${style.link} ${pathName === path && style["active-link"]}`}
+    >
+      <div>{icon}</div>
+      <div className="flex flex-col">
+        <span className="text-lg font-bold leading-5 text-white">{title}</span>
+        <span className="text-sm text-white/50 hidden md:block">
+          {subTitle}
+        </span>
+      </div>
+    </Link>
+  );
+}
